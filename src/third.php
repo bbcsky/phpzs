fb1a26ac754279b0e9695653e195ae974c264356
zhouxiaowen <zhouxiaowen@xiandanjia.com>
2017-08-22 16:52:24 +0800

    phpzs init

---
+++ src/third.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouxiaowen
+ * Date: 2017/1/5
+ * Time: 下午3:24
+ */
+
+//急切加载
+//require(dirname(__FILE__) . '/third/aliyun-mns/mns-autoloader.php');
+//require(dirname(__FILE__) . '/third/aliyun-oss/autoload.php');
+//惰性加载
+//RD::addNameSpace('bbcsky','@RD/third/bbcsky/');
